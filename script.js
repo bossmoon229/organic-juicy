f968d7a6410fa09e50982fbb0f5f7445dd03bb92
bossmoon229 <alinaignatich@gmail.com>
2025-01-27 18:18:29 +0200

    add slider and dot change img

--- script.js
+++ script.js
@@ -26,9 +26,42 @@ function showSlideArrows(n = 0) {
   if (slideIndex < 0) slideIndex = slidesArrow.length - 1;
   if (slideIndex > slidesArrow.length - 1) slideIndex = 0;
 
-
   slidesArrow.forEach((item) => (item.style.display = "none"));
   slidesArrow[slideIndex].style.display = "block";
 }
 
 showSlideArrows();
+
+// Slider with dots
+
+const sliderDots = document.querySelector(".slider-dots"),
+  slidesDots = sliderDots.querySelectorAll(".slider-dots__item"),
+  wrapperDots = sliderDots.querySelector(".slider-dots__nav");
+
+const dots = [];
+
+for (let i = 0; i < slidesDots.length; i++) {
+  const dot = document.createElement("button");
+
+  dot.dataset.slideTo = i;
+
+  dot.classList.add("slider-dots__nav-item");
+
+  if (i == 0) dot.classList.add("slider-dots__nav-item--active");
+
+  if (i != 0) slidesDots[i].style.display = "none";
+    dot.addEventListener('click', showSlideDots);
+
+  wrapperDots.append(dot);
+  dots.push(dot)
+}
+
+function showSlideDots(event) {
+    const slideTo = event.target.dataset.slideTo;
+
+    slidesDots.forEach(item => item.style.display = 'none');
+    slidesDots[slideTo].style.display = 'block';
+
+    dots.forEach(dot => dot.classList.remove('slider-dots__nav-item--active'));
+    event.target.classList.add('slider-dots__nav-item--active');
+}
